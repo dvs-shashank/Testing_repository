ae85463df841d1f03ed0d67db3964b63dec596a0
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-02 12:56:49 +0530

    " -> 0 of 1 passed. pylint: 6.00/10 "

--- cspp1-practice/m3/compare_AB.py
+++ cspp1-practice/m3/compare_AB.py
@@ -1,5 +1,27 @@
-varA=input("enter string or integer")
-varB=input("enter string or integer")
-if(type(varA)==type(string()) or type(varB)==type(string())):
-    print("hello")
+print("enter your option")
+varA=none
+varB=none
+option=int(input("1)only integer 2)string or integer 3)integer or string")
+if(option==1):
+               varA=int(input("enter  integer"))
+               varB=input("enter integer")
+               if(varA>varB):
+                   print("bigger")
+               elif(varA<varB):
+                   print("smaller")
+               elif(varA==varB):
+                   print("equal")
+if(option==2):
+               varA=input("enter string ")
+               varB=int(input("enter int "))
+               if(type(varA)==type(str()) or type(varB)==type(str())):
+                   print("Strings involved")
+
+if(option==3):
+               varA=int(input("enter int "))
+               varB=input("enter string ")
+               if(type(varA)==type(str()) or type(varB)==type(str())):
+                   print("Strings involved")
+
+
    
