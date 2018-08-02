28810c8647c2df644d8e3c6dd58ae00bdb13fe45
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-02 19:10:49 +0530

    varibale comparisions

--- cspp1-practice/m3/compare_AB.py
+++ cspp1-practice/m3/compare_AB.py
@@ -1,27 +1,20 @@
-print("enter your option")
-varA=none
-varB=none
-option=int(input("1)only integer 2)string or integer 3)integer or string")
+option=int(input("1)int 2)string and int 3)int and string"))
 if(option==1):
-               varA=int(input("enter  integer"))
-               varB=input("enter integer")
-               if(varA>varB):
-                   print("bigger")
-               elif(varA<varB):
-                   print("smaller")
-               elif(varA==varB):
-                   print("equal")
+    varA=int(input("enter number"))
+    varB=int(input("enter number"))
+    if(varA>varB):
+        print("bigger")
+    elif(varA<varB):
+        print("small")
+    else:
+        print("equal")
 if(option==2):
-               varA=input("enter string ")
-               varB=int(input("enter int "))
-               if(type(varA)==type(str()) or type(varB)==type(str())):
-                   print("Strings involved")
-
+    varA=input("enter string")
+    varB=int(input("number"))
+    if(type(varA)==type(str()) or type(varB)==type(str())):
+        print("string involved")
 if(option==3):
-               varA=int(input("enter int "))
-               varB=input("enter string ")
-               if(type(varA)==type(str()) or type(varB)==type(str())):
-                   print("Strings involved")
-
-
-   
+    varA=int(input("number"))
+    varB=input("string")
+    if(type(varA)==type(str()) or type(varB)==type(str())):
+        print("string involved")
