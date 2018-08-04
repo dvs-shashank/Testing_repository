79114fae6f542788d8fbc691c758c44dbcf75009
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 11:08:02 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/m6/p3/digit_product.py
@@ -0,0 +1,12 @@
+"""program to find product of digits"""
+N = int(input())
+I = 1
+COUNT = 0
+PROD = 1
+REM = 0
+while N > 0:
+    REM = N % 10
+    PROD = PROD * REM
+    N = N // 10
+    I = I + 1
+print(PROD)
