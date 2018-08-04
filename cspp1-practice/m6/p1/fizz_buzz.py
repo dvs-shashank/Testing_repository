e86a7461c89c2b5d400f1baa62a69f9b5f9b2f71
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:43:44 +0530

    " -> 7 of 7 passed. pylint: 5.83/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -1,12 +1,13 @@
+"""This is a fizz buzz program"""
 N = int(input())
 I = 1
-while(I <= N):
-    if(I % 3 == 0 and I % 5 == 0): 
+while (I <= N):
+    if (I % 3 == 0 and I % 5 == 0): 
         print("Fizz")
         print("Buzz")
-    elif(I % 3 == 0):
+    elif (I % 3 == 0):
         print("Fizz")
-    elif(I % 5 == 0):
+    elif (I % 5 == 0):
         print("Buzz")
     else:
         print(I)
