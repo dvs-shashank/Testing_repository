bc7e0c65c1de72c3e21ecf986905628dc6ca849e
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:36:16 +0530

    " -> 7 of 7 passed. pylint: 5.00/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -1,6 +1,9 @@
+"""take input from user"""
 N = int(input())
 I = 1
+"""checking while conditions"""
 while(I <= N):
+    """checking if conditions"""
     if(I % 3 == 0 and I % 5 == 0): 
         print("Fizz")
         print("Buzz")
@@ -11,6 +14,7 @@ while(I <= N):
     else:
         print(I)
     I = I + 1
+
 
 
     
