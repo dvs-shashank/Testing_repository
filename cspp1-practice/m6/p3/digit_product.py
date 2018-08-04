4b8108276436461210c433338e65ede2a09ea28e
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 11:13:37 +0530

    " -> 2 of 7 passed. pylint: 7.50/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -6,7 +6,8 @@ PROD = 1
 REM = 0
 while N > 0:
     REM = N % 10
-    PROD = PROD * REM
-    N = N // 10
+    if( REM > 0):
+        PROD = PROD * REM
+    N = N // 10    
     I = I + 1
 print(PROD)
