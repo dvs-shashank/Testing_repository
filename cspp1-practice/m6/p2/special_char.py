a8aedbbdbdfc41f4bb07c7a0b237149b71185a27
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:48:28 +0530

    " -> 7 of 7 passed. pylint: 2.00/10 "

--- cspp1-practice/m6/p2/special_char.py
+++ cspp1-practice/m6/p2/special_char.py
@@ -10,7 +10,7 @@ LE = len(STR)
 I = 0
 X = ""
 while(I < LE):
-    if (STR[I]=='!' or STR[I]=='@' or STR[I]=='#' or STR[I]=='$' or STR[I]=='%' or STR[I]=='^' or STR[I]=='&' or STR[I]=='*'):
+    if (STR[I] == '!' or STR[I] == '@' or STR[I] == '#' or STR[I] == '$' or STR[I] == '%' or STR[I] == '^' or STR[I] == '&' or STR[I] == '*'):
         X = X + " "
     else:
         X = X + STR[I]
