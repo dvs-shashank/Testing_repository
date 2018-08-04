a5a04eb7762104d79786780fdfe52da54eb0ff52
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:39:00 +0530

    " -> 7 of 7 passed. pylint: 5.71/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -2,14 +2,14 @@
 N = int(input())
 I = 1
 """checking while conditions"""
-while( I <= N ):
+while(I <= N):
     """checking if conditions"""
-    if( I % 3 == 0 and I % 5 == 0 ): 
+    if(I % 3 == 0 and I % 5 == 0): 
         print("Fizz")
         print("Buzz")
-    elif( I % 3 == 0 ):
+    elif(I % 3 == 0):
         print("Fizz")
-    elif( I % 5 == 0 ):
+    elif(I % 5 == 0):
         print("Buzz")
     else:
         print(I)
