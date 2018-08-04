d6cb8bcc49f3bcb648c45c3e2b3606e69ca7d4b2
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:56:10 +0530

    " -> 1 of 7 passed. pylint: 7.14/10 "

--- cspp1-practice/m6/p2/special_char.py
+++ cspp1-practice/m6/p2/special_char.py
@@ -9,7 +9,7 @@ while (I < LE):
     if (STR[I] == '$' or STR[I] == '%' or STR[I] == '^'):
         X =X + " "
     if (STR[I] == '&' or STR[I] == '*'):
-        X =X + " "
+        X = X + " "
     else:
         X = X + STR[I]
     I = I +1
