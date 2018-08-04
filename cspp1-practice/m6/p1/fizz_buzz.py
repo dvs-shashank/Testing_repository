0c6145d518d4973f8f753c6b0aaa455826871a7a
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-04 10:33:14 +0530

    " -> 7 of 7 passed. pylint: 4.17/10 "

--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -1,13 +1,12 @@
-N = int(input())'''taking the input from user'''
+N = int(input())
 I = 1
-'''Iterating through the loop'''
 while(I <= N):
-    if(I % 3 == 0 and I % 5 == 0): '''checking if it is divisible by both 3 and 5'''
+    if(I % 3 == 0 and I % 5 == 0): 
         print("Fizz")
         print("Buzz")
-    elif(I % 3 == 0):'''checking if divisible by 3'''
+    elif(I % 3 == 0):
         print("Fizz")
-    elif(I % 5== 0):'''checking if divisible by 5'''
+    elif(I % 5== 0):
         print("Buzz")
     else:
         print(I)
