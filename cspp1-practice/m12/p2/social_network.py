8c9d00a2f3ed81eddf33277319a8d42d6b55e187
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-11 12:34:38 +0530

    " -> 0 of 2 passed. pylint: 3.46/10 "

--- cspp1-practice/m12/p2/social_network.py
+++ cspp1-practice/m12/p2/social_network.py
@@ -42,7 +42,7 @@ def delete_person(network, arg1):
     # remove the pass below and start writing your code
     if arg1 in network:
         if 'd' in network:
-            del network[d]
+            del network['d']
         del network[arg1]
     return network
 
