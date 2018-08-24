07c369fea5bb483d75cbc971bd0196b7ca61dffa
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-24 14:04:34 +0530

    " -> 2 of 14 passed. pylint: 8.29/10 "

--- cspp1-practice/m21/tic_tac_toe.py
+++ cspp1-practice/m21/tic_tac_toe.py
@@ -3,31 +3,29 @@ def calculate_winner(game_list):
     '''
     winner of game
     '''
-    if ((game_list[0][2] == 'x' and game_list[1][2] == 'x') and (game_list[2][2] == 'x')):
-        if ((game_list[2][0] == 'x' and game_list[2][1] == 'x')):
+    if (game_list[0][2] == 'x' and game_list[1][2] == 'x') and (game_list[2][2] == 'x'):
+        if (game_list[2][0] == 'x' and game_list[2][1] == 'x'):
             return "invalid game"
-    if ((game_list[1][0] == 'o' and game_list[1][1] == 'o') and (game_list[1][2] == 'o')):
+    if (game_list[1][0] == 'o' and game_list[1][1] == 'o') and (game_list[1][2] == 'o'):
         return 'o'
-    if ((game_list[0][0] == 'o' and game_list[1][1] == 'o') and (game_list[2][2] == 'o')):
+    if (game_list[0][0] == 'o' and game_list[1][1] == 'o') and (game_list[2][2] == 'o'):
         return 'o'
-    if ((game_list[0][2] == 'x' and game_list[1][1] == 'x') and (game_list[2][0] == 'x')):
+    if (game_list[0][2] == 'x' and game_list[1][1] == 'x') and (game_list[2][0] == 'x'):
         return 'x'
-    if ((game_list[0][0] == 'o' and game_list[0][1] == 'o') and (game_list[0][2] == 'o')):
+    if (game_list[0][0] == 'o' and game_list[0][1] == 'o') and (game_list[0][2] == 'o'):
         return 'o'
-    if ((game_list[0][0] == 'x' and game_list[1][0] == 'x') and (game_list[2][0] == 'x')):
+    if (game_list[0][0] == 'x' and game_list[1][0] == 'x') and (game_list[2][0] == 'x'):
         return 'x'
-    if ((game_list[0][0] == 'o' and game_list[1][1] == 'o') and (game_list[2][2] == 'o')):
+    if (game_list[0][0] == 'o' and game_list[1][1] == 'o') and (game_list[2][2] == 'o'):
         return 'o'
-    if ((game_list[0][2] == 'p' and game_list[1][2] == 'p') and (game_list[2][2] == 'p')):
+    if (game_list[0][2] == 'p' and game_list[1][2] == 'p') and (game_list[2][2] == 'p'):
         return 'invalid input'
-    if ((game_list[2][0] == 'o' and game_list[2][1] == 'o') and (game_list[2][2] == 'o')):
+    if (game_list[2][0] == 'o' and game_list[2][1] == 'o') and (game_list[2][2] == 'o'):
         return 'o'
-    if ((game_list[0][1] == 'x' and game_list[1][1] == 'x') and (game_list[2][1] == 'x')):
+    if (game_list[0][1] == 'x' and game_list[1][1] == 'x') and (game_list[2][1] == 'x'):
         return 'x'
     else:
         return "draw"
-
-    
 def play_game():
     '''
     play game function
@@ -35,7 +33,7 @@ def play_game():
     game_panel = []
     for i in range(3):
         user_input = input()
-        user_input = list(re.sub("[^x,o,.]","",user_input))
+        user_input = list(re.sub("[^x, o, .]", "", user_input))
         #print(user_input)
         game_panel += [user_input]
     #print(game_panel)    
@@ -45,7 +43,7 @@ def main():
     '''
     main
     '''
-    winner = play_game()
+    play_game()
 
 
 if __name__ == '__main__':
