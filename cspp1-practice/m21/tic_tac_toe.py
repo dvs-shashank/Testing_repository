ce7e04651768f30302e647e7ab75e44e1cbd9c3f
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-24 13:44:57 +0530

    " -> 8 of 14 passed. pylint: -4.12/10 "

--- cspp1-practice/m21/tic_tac_toe.py
+++ cspp1-practice/m21/tic_tac_toe.py
@@ -4,7 +4,7 @@ def calculate_winner(game_list):
         return 'o'
     if ((game_list[0][0]=='o' and game_list[1][1]=='o') and (game_list[2][2]=='o')):
         return 'o'
-    if ((game_list[0][3]=='x' and game_list[1][1]=='x') and (game_list[2][0]=='x')):
+    if ((game_list[0][2]=='x' and game_list[1][1]=='x') and (game_list[2][0]=='x')):
         return 'x'
     if ((game_list[0][0]=='o' and game_list[0][1]=='o') and (game_list[0][2]=='o')):
         return 'o'
@@ -29,7 +29,7 @@ def play_game():
     for i in range(3):
         user_input = input()
         user_input = list(re.sub("[^x,o,.]","",user_input))
-        print(user_input)
+        #print(user_input)
         game_panel += [user_input]
     #print(game_panel)    
     print(calculate_winner(game_panel))
