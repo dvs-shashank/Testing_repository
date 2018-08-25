2c117239a3d7e5d8aa5503c2922bd1365277447e
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-08-25 12:42:28 +0530

    " -> 0 of 26 passed. pylint: 1.21/10 "

--- cspp1-practice/m22/Check Sudoku/check_sudoku.py
+++ cspp1-practice/m22/Check Sudoku/check_sudoku.py
@@ -8,19 +8,43 @@
     satisfies all the sudoku rules given in the statement above.
 '''
 
+
+def data_validation(sudoku):
+    list_num = ['1','2','3','4','5','6','7','8','9']
+    count = 0
+    for i in range(len(sudoku)):
+        temp_list = list_num[:]
+        #print("arey")
+        for j in range(len(sudoku[0])):
+            #print("jerey")
+            if sudoku[i][j] in temp_list:
+                temp_list.remove(sudoku[i][j])
+                print(temp_list)
+        if len(temp_list) == 0:
+            count += 1
+            print(count)
+    if count != 9:
+        return False
+    
+    
+    
+
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
     #print(sudoku)
-    num_list = ['0','1','2','3','4','5','6','7','8','9']
-    for i in sudoku:
-        for j in i:
-            if j not in num_list:
-                return False
-    return True
-
+    global num_list
+    num_list = ['1','2','3','4','5','6','7','8','9']
+    flag = data_validation(sudoku)
+    if flag== True:
+        for i in sudoku:
+            for j in i:
+                if j not in num_list:
+                    return False
+        return True
+    return False
 def main():
     '''
         main function to read input sudoku from console
